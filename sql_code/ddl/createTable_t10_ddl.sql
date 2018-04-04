4c4708ca0eb05f9537f012e876c7835dd8123a4d
riaki <iakivets@gmail.com>
2018-04-04 17:02:52 +0300

    pkg 10

---
+++ sql_code/ddl/createTable_t10_ddl.sql
@@ -0,0 +1,7 @@
+CREATE TABLE ads.t10(
+   ID INT PRIMARY KEY     NOT NULL,
+   NAME           TEXT    NOT NULL,
+   AGE            INT     NOT NULL,
+   ADDRESS        CHAR(50),
+   SALARY         REAL
+   );
