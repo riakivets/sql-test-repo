65277fc2eb1b7e058cf7d3669ba6fbd9708eaeea
Roman Iakivets <iakivets@gmail.com>
2020-01-21 15:17:48 +0200

    useremail with drop

--- sql_code/function/useremail.sql
+++ sql_code/function/useremail.sql
@@ -1,8 +1,1 @@
-CREATE FUNCTION [dbo].[useremail]()
-RETURNS VARCHAR(100)
-AS
-BEGIN
-DECLARE @rtEmail VARCHAR
-SET @rtEmail='random@datical.com'
-RETURN @rtEmail
-END
+DROP FUNCTION [dbo].[useremail]
